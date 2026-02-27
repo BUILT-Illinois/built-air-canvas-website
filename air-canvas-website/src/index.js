e3a92f13e5106b4262a073a12187eb1e6fbfcd82
samev <samevangelista07@icloud.com>
2026-02-26 18:40:13 -0600

    Added Routing

--- air-canvas-website/src/index.js
+++ air-canvas-website/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Data from './pages/Data';
+import About from './pages/About';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/data' element={<Data/>}/>
+    </Routes>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ air-canvas-website/src/pages/Data.jsx
@@ -0,0 +1,7 @@
+export default function Data() {
+    return(
+        <div>
+            <h1>Data Page</h1>
+        </div>
+    );
+}
