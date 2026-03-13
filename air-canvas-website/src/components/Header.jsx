718e622a5a0c1cc90fb7413ddfe1547ff0a0d1ad
samev <samevangelista07@icloud.com>
2026-03-12 19:27:45 -0500

    Cleaned Header and Started About

--- air-canvas-website/src/components/Header.jsx
+++ air-canvas-website/src/components/Header.jsx
@@ -5,21 +5,26 @@ function Header() {
   return (
     <div className="header">
       <div className="container">
-        <Link className="image-link" to="/">
-          <img
-            className="built-img"
-            alt="built logo"
-            src="https://built-illinois.org/built-logo.png"
-          />
-        </Link>
-        <h1>Air Canvas</h1>
-
-        <Link to="/about">
-          <h2 className="header-button">About</h2>
-        </Link>
-        <Link to="/data">
-          <h2 className="header-button">Data</h2>
-        </Link>
+        <div className="img-container">
+          <Link className="image-link" to="/">
+            <img
+              className="built-img"
+              alt="built logo"
+              src="https://built-illinois.org/built-logo.png"
+            />
+          </Link>
+          <h1 className="title">Air Canvas</h1>
+        </div>
+        
+        
+        <div className="right-container">
+          <Link to="/about">
+            <h2 className="header-button">About</h2>
+          </Link>
+          <Link to="/data">
+            <h2 className="header-button">Data</h2>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- air-canvas-website/src/pages/About.jsx
+++ air-canvas-website/src/pages/About.jsx
@@ -1,7 +1,23 @@
+import "../assets/About.css";
+
 export default function About() {
-    return (
-        <div>
-            <h1>About Page</h1>
+  return (
+    <div>
+      <div className="about-container">
+        <div className="left">
+          <h1>Overview</h1>
+          <h2>
+            B[U]ILT Air Canvas is an interactive Engineering Open House exhibit
+            that lets visitors “draw in the air” using Disney-style ESP32 wands
+            or hand gestures tracked by a webcam, turning motion into real-time
+            visualizations on a screen
+          </h2>
         </div>
-    );
+
+        <div className="right">
+
+        </div>
+      </div>
+    </div>
+  );
 }
