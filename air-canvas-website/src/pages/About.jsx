22b5519064b2272057a4805c3f489b71a2f7fe4f
Budrey <audreyramirez06@gmail.com>
2026-04-10 00:32:28 -0500

    finished up pretty much, just need to add proper text and images & profiles

--- air-canvas-website/src/pages/About.jsx
+++ air-canvas-website/src/pages/About.jsx
@@ -5,17 +5,17 @@ export default function About() {
     <div>
       <div className="about-container">
         <div className="left">
-          <h1>Overview</h1>
-          <h2>
-            B[U]ILT Air Canvas is an interactive Engineering Open House exhibit
-            that lets visitors “draw in the air” using Disney-style ESP32 wands
-            or hand gestures tracked by a webcam, turning motion into real-time
-            visualizations on a screen
-          </h2>
-        </div>
-        <div className="right">
-          <div className="curved-box"></div>
-        </div>
+            <h1>Overview</h1>
+            <h2>
+              B[U]ILT Air Canvas is an interactive Engineering Open House exhibit
+              that lets visitors “draw in the air” using Disney-style ESP32 wands
+              or hand gestures tracked by a webcam, turning motion into real-time
+              visualizations on a screen
+            </h2>
+          </div>
+          <div className="right">
+            <div className="curved-box"></div>
+          </div>
         <div className="left">
           <div className="curved-box"></div>
         </div>
